b919d8e395ce5e4d3f77d69aaf263bb87f563860
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2024-03-22 15:24:11 +0300

    add underlinedTextField subclass

--- BirthdayReminder/HelperClasses/UnderlinedTextField.swift
+++ BirthdayReminder/HelperClasses/UnderlinedTextField.swift
@@ -8,4 +8,29 @@
 import UIKit
 
 class UnderlinedTextField: UITextField {
+    let underlineLayer = CALayer()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.layer.addSublayer(underlineLayer)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        setupUnderlinedLayer()
+    }
+
+    private func setupUnderlinedLayer() {
+        var frame = self.bounds
+        frame.origin.y = frame.height - 1
+        frame.size.height = 1
+
+        underlineLayer.frame = frame
+        underlineLayer.backgroundColor = UIColor.gray.cgColor
+    }
 }
