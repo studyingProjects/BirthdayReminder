905ad2ed09773f5714157519696da58eb95bf848
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2024-03-23 18:23:33 +0300

    add EditProfile simple layout

--- BirthdayReminder/Constants/Sizes.swift
+++ BirthdayReminder/Constants/Sizes.swift
@@ -11,7 +11,7 @@ enum Sizes {
     enum Small {
         static let padding: CGFloat = 8
         static let width: CGFloat = 50
-        static let height: CGFloat = 44
+        static let height: CGFloat = 35
         static let cornerRadius: CGFloat = 10
 
         static let borderWidth: CGFloat = 1
@@ -20,7 +20,7 @@ enum Sizes {
     enum Medium {
         static let padding: CGFloat = 16
         static let width: CGFloat = 100
-        static let height: CGFloat = 88
+        static let height: CGFloat = 70
         static let cornerRadius: CGFloat = 20
 
         static let borderWidth: CGFloat = 3
@@ -29,7 +29,7 @@ enum Sizes {
     enum Large {
         static let padding: CGFloat = 32
         static let width: CGFloat = 200
-        static let height: CGFloat = 136
+        static let height: CGFloat = 140
         static let cornerRadius: CGFloat = 30
 
         static let borderWidth: CGFloat = 6
@@ -38,7 +38,7 @@ enum Sizes {
     enum XLarge {
         static let padding: CGFloat = 64
         static let width: CGFloat = 400
-        static let height: CGFloat = 352
+        static let height: CGFloat = 280
         static let cornerRadius: CGFloat = 40
 
         static let borderWidth: CGFloat = 9
