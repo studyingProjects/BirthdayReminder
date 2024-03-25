39abbba5a8c2c2f8b14d4e902ad2a2f98ec3eee1
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2024-03-25 15:49:09 +0300

    editProfile constraints separation

--- BirthdayReminder/Modules/EdtiProfile/View/EditProfileView.swift
+++ BirthdayReminder/Modules/EdtiProfile/View/EditProfileView.swift
@@ -15,46 +15,46 @@ class EditProfileView: UIView, UINavigationBarDelegate {
     // MARK: - Properties
     weak var delegate: EditProfileViewDelegate?
 
-    private lazy var navigationBar = EditProfileNavigationBar()
-    private lazy var profileImageView = UIImageView.getProfileImageView()
-    private lazy var profileLabel = UILabel(
+    lazy var navigationBar = EditProfileNavigationBar()
+    lazy var profileImageView = UIImageView.getProfileImageView()
+    lazy var profileLabel = UILabel(
         text: "Edit photo",
         font: .appMediumFont,
         color: .systemBlue,
         aligment: .center
     )
-    private lazy var bodyView = UIView(cornerRadius: Sizes.Small.cornerRadius)
+    lazy var bodyView = UIView(cornerRadius: Sizes.Small.cornerRadius)
     // Name
-    private lazy var nameLabel = UILabel(text: "Name", font: .appMediumFont, color: .systemBlue)
-    private lazy var nameTextField = UnderlinedTextField(
+    lazy var nameLabel = UILabel(text: "Name", font: .appMediumFont, color: .systemBlue)
+    lazy var nameTextField = UnderlinedTextField(
         font: .appMediumFont,
         placeholder: "Fill in the name",
         keyboardType: .default
     )
     // Date
-    private lazy var dateLabel = UILabel(text: "Date", font: .appMediumFont, color: .systemBlue)
-    private lazy var dateTextField = UnderlinedTextField(
+    lazy var dateLabel = UILabel(text: "Date", font: .appMediumFont, color: .systemBlue)
+    lazy var dateTextField = UnderlinedTextField(
         font: .appMediumFont,
         placeholder: "Fill in the date",
         keyboardType: .default
     )
     // Age
-    private lazy var ageLabel = UILabel(text: "Age", font: .appMediumFont, color: .systemBlue)
-    private lazy var ageTextField = UnderlinedTextField(
+    lazy var ageLabel = UILabel(text: "Age", font: .appMediumFont, color: .systemBlue)
+    lazy var ageTextField = UnderlinedTextField(
         font: .appMediumFont,
         placeholder: "To add",
         keyboardType: .default
     )
     // Sex
-    private lazy var sexLabel = UILabel(text: "Sex", font: .appMediumFont, color: .systemBlue)
-    private lazy var sexTextField = UnderlinedTextField(
+    lazy var sexLabel = UILabel(text: "Sex", font: .appMediumFont, color: .systemBlue)
+    lazy var sexTextField = UnderlinedTextField(
         font: .appMediumFont,
         placeholder: "To add",
         keyboardType: .default
     )
     // Instagram
-    private lazy var instLabel = UILabel(text: "Instagram", font: .appMediumFont, color: .systemBlue)
-    private lazy var instTextField = UnderlinedTextField(
+    lazy var instLabel = UILabel(text: "Instagram", font: .appMediumFont, color: .systemBlue)
+    lazy var instTextField = UnderlinedTextField(
         font: .appMediumFont,
         placeholder: "To add",
         keyboardType: .default
@@ -64,6 +64,7 @@ class EditProfileView: UIView, UINavigationBarDelegate {
         super.init(frame: frame)
 
         setupView()
+        // EditProfileView+Constraints.swift
         setupConstraints()
         navigationBar.customDelegate = self
     }
@@ -112,7 +113,14 @@ extension EditProfileView: UITextFieldDelegate {
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         if textField.isEqual(instTextField) {
             instTextFieldBeginEditing()
+        } else if textField.isEqual(ageTextField) {
+            ageTextFieldBeginEditing()
+        } else if textField.isEqual(dateTextField) {
+            dateTextFieldBeginEditing()
+        } else if textField.isEqual(sexTextField) {
+            sexTextFieldBeginEditing()
         }
+
         return false
     }
 }
@@ -152,131 +160,11 @@ private extension EditProfileView {
 
         alert.addAction(cancelAction)
         alert.addAction(doneAction)
-        
+
         delegate?.presentInstAlert(alert)
     }
-}
-// MARK: - Constraints
-private extension EditProfileView {
-    private func setupConstraints() {
-        setupHeaderConstraints()
-        setupBodyConstraints()
-        setupNameConstraints()
-        setupDateConstraints()
-        setupAgeConstraints()
-        setupSexConstraints()
-        setupInstConstraints()
-    }
 
-    private func setupHeaderConstraints() {
-        NSLayoutConstraint.activate([
-            navigationBar.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            navigationBar.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            navigationBar.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
-            // navigationBar.heightAnchor.constraint(equalToConstant: Sizes.Small.height),
-
-            profileImageView.leadingAnchor.constraint(
-                equalTo: layoutMarginsGuide.leadingAnchor,
-                constant: Sizes.Large.padding
-            ),
-            profileImageView.trailingAnchor.constraint(
-                equalTo: layoutMarginsGuide.trailingAnchor,
-                constant: -Sizes.Large.padding
-            ),
-            profileImageView.topAnchor.constraint(
-                equalTo: navigationBar.bottomAnchor
-            ),
-            profileImageView.heightAnchor.constraint(greaterThanOrEqualToConstant: Sizes.Large.height),
-
-            profileLabel.topAnchor.constraint(
-                equalTo: profileImageView.bottomAnchor
-            ),
-            profileLabel.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor),
-            profileLabel.trailingAnchor.constraint(equalTo: profileImageView.trailingAnchor)
-        ])
-    }
-
-    private func setupBodyConstraints() {
-        NSLayoutConstraint.activate([
-            bodyView.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
-            bodyView.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
-            bodyView.topAnchor.constraint(equalTo: profileLabel.bottomAnchor),
-            bodyView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
-        ])
-    }
-
-    private func setupNameConstraints() {
-        NSLayoutConstraint.activate([
-            nameLabel.leadingAnchor.constraint(equalTo: bodyView.leadingAnchor),
-            nameLabel.trailingAnchor.constraint(equalTo: bodyView.trailingAnchor),
-            nameLabel.topAnchor.constraint(
-                equalTo: bodyView.topAnchor,
-                constant: Sizes.Small.padding
-            ),
-            nameTextField.leadingAnchor.constraint(equalTo: bodyView.leadingAnchor),
-            nameTextField.trailingAnchor.constraint(equalTo: bodyView.trailingAnchor),
-            nameTextField.topAnchor.constraint(equalTo: nameLabel.bottomAnchor),
-            nameTextField.heightAnchor.constraint(equalToConstant: Sizes.Small.height)
-        ])
-    }
-
-    private func setupDateConstraints() {
-        NSLayoutConstraint.activate([
-            dateLabel.leadingAnchor.constraint(equalTo: bodyView.leadingAnchor),
-            dateLabel.trailingAnchor.constraint(equalTo: bodyView.trailingAnchor),
-            dateLabel.topAnchor.constraint(
-                equalTo: nameTextField.bottomAnchor,
-                constant: Sizes.Small.padding
-            ),
-            dateTextField.leadingAnchor.constraint(equalTo: bodyView.leadingAnchor),
-            dateTextField.trailingAnchor.constraint(equalTo: bodyView.trailingAnchor),
-            dateTextField.topAnchor.constraint(equalTo: dateLabel.bottomAnchor),
-            dateTextField.heightAnchor.constraint(equalToConstant: Sizes.Small.height)
-        ])
-    }
-
-    private func setupAgeConstraints() {
-        NSLayoutConstraint.activate([
-            ageLabel.leadingAnchor.constraint(equalTo: bodyView.leadingAnchor),
-            ageLabel.trailingAnchor.constraint(equalTo: bodyView.trailingAnchor),
-            ageLabel.topAnchor.constraint(
-                equalTo: dateTextField.bottomAnchor,
-                constant: Sizes.Small.padding
-            ),
-            ageTextField.leadingAnchor.constraint(equalTo: bodyView.leadingAnchor),
-            ageTextField.trailingAnchor.constraint(equalTo: bodyView.trailingAnchor),
-            ageTextField.topAnchor.constraint(equalTo: ageLabel.bottomAnchor),
-            ageTextField.heightAnchor.constraint(equalToConstant: Sizes.Small.height)
-        ])
-    }
-
-    private func setupSexConstraints() {
-        NSLayoutConstraint.activate([
-            sexLabel.leadingAnchor.constraint(equalTo: bodyView.leadingAnchor),
-            sexLabel.trailingAnchor.constraint(equalTo: bodyView.trailingAnchor),
-            sexLabel.topAnchor.constraint(
-                equalTo: ageTextField.bottomAnchor,
-                constant: Sizes.Small.padding
-            ),
-            sexTextField.leadingAnchor.constraint(equalTo: bodyView.leadingAnchor),
-            sexTextField.trailingAnchor.constraint(equalTo: bodyView.trailingAnchor),
-            sexTextField.topAnchor.constraint(equalTo: sexLabel.bottomAnchor),
-            sexTextField.heightAnchor.constraint(equalToConstant: Sizes.Small.height)
-        ])
-    }
-
-    private func setupInstConstraints() {
-        NSLayoutConstraint.activate([
-            instLabel.leadingAnchor.constraint(equalTo: bodyView.leadingAnchor),
-            instLabel.trailingAnchor.constraint(equalTo: bodyView.trailingAnchor),
-            instLabel.topAnchor.constraint(
-                equalTo: sexTextField.bottomAnchor,
-                constant: Sizes.Small.padding
-            ),
-            instTextField.leadingAnchor.constraint(equalTo: bodyView.leadingAnchor),
-            instTextField.trailingAnchor.constraint(equalTo: bodyView.trailingAnchor),
-            instTextField.topAnchor.constraint(equalTo: instLabel.bottomAnchor),
-            instTextField.heightAnchor.constraint(equalToConstant: Sizes.Small.height)
-        ])
-    }
+    private func ageTextFieldBeginEditing() {}
+    private func sexTextFieldBeginEditing() {}
+    private func dateTextFieldBeginEditing() {}
 }
---
+++ BirthdayReminder/Modules/EdtiProfile/View/Extensions/EditProfileView+Constraints.swift
@@ -0,0 +1,132 @@
+//
+//  EditProfileView+Constraints.swift
+//  BirthdayReminder
+//
+//  Created by Andrei Shpartou on 25/03/2024.
+//
+
+import UIKit
+
+extension EditProfileView {
+    func setupConstraints() {
+        setupHeaderConstraints()
+        setupBodyConstraints()
+        setupNameConstraints()
+        setupDateConstraints()
+        setupAgeConstraints()
+        setupSexConstraints()
+        setupInstConstraints()
+    }
+
+    private func setupHeaderConstraints() {
+        NSLayoutConstraint.activate([
+            navigationBar.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            navigationBar.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            navigationBar.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            // navigationBar.heightAnchor.constraint(equalToConstant: Sizes.Small.height),
+
+            profileImageView.leadingAnchor.constraint(
+                equalTo: layoutMarginsGuide.leadingAnchor,
+                constant: Sizes.Large.padding
+            ),
+            profileImageView.trailingAnchor.constraint(
+                equalTo: layoutMarginsGuide.trailingAnchor,
+                constant: -Sizes.Large.padding
+            ),
+            profileImageView.topAnchor.constraint(
+                equalTo: navigationBar.bottomAnchor
+            ),
+            profileImageView.heightAnchor.constraint(greaterThanOrEqualToConstant: Sizes.Large.height),
+
+            profileLabel.topAnchor.constraint(
+                equalTo: profileImageView.bottomAnchor
+            ),
+            profileLabel.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor),
+            profileLabel.trailingAnchor.constraint(equalTo: profileImageView.trailingAnchor)
+        ])
+    }
+
+    private func setupBodyConstraints() {
+        NSLayoutConstraint.activate([
+            bodyView.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+            bodyView.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
+            bodyView.topAnchor.constraint(equalTo: profileLabel.bottomAnchor),
+            bodyView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+
+    private func setupNameConstraints() {
+        NSLayoutConstraint.activate([
+            nameLabel.leadingAnchor.constraint(equalTo: bodyView.leadingAnchor),
+            nameLabel.trailingAnchor.constraint(equalTo: bodyView.trailingAnchor),
+            nameLabel.topAnchor.constraint(
+                equalTo: bodyView.topAnchor,
+                constant: Sizes.Small.padding
+            ),
+            nameTextField.leadingAnchor.constraint(equalTo: bodyView.leadingAnchor),
+            nameTextField.trailingAnchor.constraint(equalTo: bodyView.trailingAnchor),
+            nameTextField.topAnchor.constraint(equalTo: nameLabel.bottomAnchor),
+            nameTextField.heightAnchor.constraint(equalToConstant: Sizes.Small.height)
+        ])
+    }
+
+    private func setupDateConstraints() {
+        NSLayoutConstraint.activate([
+            dateLabel.leadingAnchor.constraint(equalTo: bodyView.leadingAnchor),
+            dateLabel.trailingAnchor.constraint(equalTo: bodyView.trailingAnchor),
+            dateLabel.topAnchor.constraint(
+                equalTo: nameTextField.bottomAnchor,
+                constant: Sizes.Small.padding
+            ),
+            dateTextField.leadingAnchor.constraint(equalTo: bodyView.leadingAnchor),
+            dateTextField.trailingAnchor.constraint(equalTo: bodyView.trailingAnchor),
+            dateTextField.topAnchor.constraint(equalTo: dateLabel.bottomAnchor),
+            dateTextField.heightAnchor.constraint(equalToConstant: Sizes.Small.height)
+        ])
+    }
+
+    private func setupAgeConstraints() {
+        NSLayoutConstraint.activate([
+            ageLabel.leadingAnchor.constraint(equalTo: bodyView.leadingAnchor),
+            ageLabel.trailingAnchor.constraint(equalTo: bodyView.trailingAnchor),
+            ageLabel.topAnchor.constraint(
+                equalTo: dateTextField.bottomAnchor,
+                constant: Sizes.Small.padding
+            ),
+            ageTextField.leadingAnchor.constraint(equalTo: bodyView.leadingAnchor),
+            ageTextField.trailingAnchor.constraint(equalTo: bodyView.trailingAnchor),
+            ageTextField.topAnchor.constraint(equalTo: ageLabel.bottomAnchor),
+            ageTextField.heightAnchor.constraint(equalToConstant: Sizes.Small.height)
+        ])
+    }
+
+    private func setupSexConstraints() {
+        NSLayoutConstraint.activate([
+            sexLabel.leadingAnchor.constraint(equalTo: bodyView.leadingAnchor),
+            sexLabel.trailingAnchor.constraint(equalTo: bodyView.trailingAnchor),
+            sexLabel.topAnchor.constraint(
+                equalTo: ageTextField.bottomAnchor,
+                constant: Sizes.Small.padding
+            ),
+            sexTextField.leadingAnchor.constraint(equalTo: bodyView.leadingAnchor),
+            sexTextField.trailingAnchor.constraint(equalTo: bodyView.trailingAnchor),
+            sexTextField.topAnchor.constraint(equalTo: sexLabel.bottomAnchor),
+            sexTextField.heightAnchor.constraint(equalToConstant: Sizes.Small.height)
+        ])
+    }
+
+    private func setupInstConstraints() {
+        NSLayoutConstraint.activate([
+            instLabel.leadingAnchor.constraint(equalTo: bodyView.leadingAnchor),
+            instLabel.trailingAnchor.constraint(equalTo: bodyView.trailingAnchor),
+            instLabel.topAnchor.constraint(
+                equalTo: sexTextField.bottomAnchor,
+                constant: Sizes.Small.padding
+            ),
+            instTextField.leadingAnchor.constraint(equalTo: bodyView.leadingAnchor),
+            instTextField.trailingAnchor.constraint(equalTo: bodyView.trailingAnchor),
+            instTextField.topAnchor.constraint(equalTo: instLabel.bottomAnchor),
+            instTextField.heightAnchor.constraint(equalToConstant: Sizes.Small.height)
+        ])
+    }
+}
